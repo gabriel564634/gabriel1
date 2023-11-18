578f959acaefa6e78c77e2ae997ac109b0f345d7
gabriel564634 <matosg683@gmail.com>
2023-11-17 21:46:52 -0300

    commit-execicio-01

---
+++ exercicio-01.py
@@ -0,0 +1,31 @@
+
+#operadores matematicos 
+soma= 6+2 
+print (soma)
+
+subtracao= 6-2
+print (subtracao)
+
+divisao= 6/2
+print (divisao)
+
+mutiplicacao= 6*2
+print (mutiplicacao)
+
+ #operacao de comparacao 
+ 
+igual= 6==2
+print (igual)
+
+diferente= 6!= 2 
+print (diferente)
+
+maior= 6>2
+print(maior)
+
+menor= 6<2
+#operradores logicos 
+e= maior and menor 
+a= menor and maior 
+ou= not True 
+print (e, ou, a)

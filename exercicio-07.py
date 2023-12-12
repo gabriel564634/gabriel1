123101ea05be8fddd0ce10931918b020ad852d6c
gabriel564634 <151092305+gabriel564634@users.noreply.github.com>
2023-12-12 20:14:49 -0300

    Update exercicio-07.py

--- exercicio-07.py
+++ exercicio-07.py
@@ -1,16 +1,28 @@
 
-import pandas as pd
-
 dados = {
-    'Nome': ['Lucas','Samuel','Davi','Jadson'],
-    'Idade': [22,19,18,21],
-    'Cidade': ['Camaçari','São Paulo','Salvador','Rio de Janeiro'],
+    'mes' : ['Janeiro', 'Janeiro', 'Janeiro', 'Fevereiro', 'Fevereiro'],
+    'Produtos' : ['camiseta',  'tenis', 'bota', 'camiseta', 'calça'],
+    'Quantidade' : [40,70,60,50,80]
+    
 }
 
-#Conversão para pandas "tabela = pd.DataFrame(dados)"
 df = pd.DataFrame(dados)
-#print (tabela)
+v_janeiro = df[df['mes'] == 'Janeiro']
+pmv_janeiro = v_janeiro['Quantidade'].idxmax()
+p_janeiro = df.loc[pmv_janeiro, 'Produtos']
+procentagem_janeiro = df.loc[pmv_janeiro, 'Quantidade'] / v_janeiro['Quantidade'].sum() * 100
 
-for dado in df.values:
-    #print(dado)
+print(f"\nProduto mais vendido em janeiro: ", p_janeiro, "com ", procentagem_janeiro, "% das vendas\n")
+
+vc_janeiro = df[(df['Produtos'] == 'camiseta') & (df['mes'] == 'Janeiro')]['Quantidade'].values[0]
+vc_fervereiro = df[(df['Produtos'] == 'camiseta') & (df['mes'] == 'Fevereiro')]['Quantidade'].values[0]
+
+aumento_percentual = ((vc_fervereiro - vc_janeiro) / vc_janeiro) * 100
+
+print("\nAumento percentual nas vendas de camisetas de janeiro para fevereiro: ", aumento_percentual, "%\n")
+
+pmv_geral= df.groupby('produto')['Quantidade'].sum().idxmax()
+tv_geral = df.groupby('Produto')['Quantidade'].sum().max()
+
+print(f"\nProduto mais vendido em geral: ", {tv_geral}, "com ", pmv_geral, "% das vendas\n")
     print(dado[0],dado[1],dado[2])
